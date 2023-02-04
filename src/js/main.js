39fdd19ecc1150f547cf0441ca9745056a62a68f
Roma <92884248+saipovx@users.noreply.github.com>
2023-02-04 03:48:24 +0300

    ok

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('.main__body')
+let white = document.querySelector('.white')
+let btn = document.querySelector('.btn')
+
+btn.addEventListener('click', function() {
+    body.classList.toggle('white');
+})
